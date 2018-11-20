58c47543329a359032ab4cdebdfd1d9f57bdba0d
peabnuts123 <peabnuts123@gmail.com>
2018-11-20 14:14:56 +1300

    Update execAsync to prioritise stderr but fall back to stdout or a hard-coded error message

--- src/util/execAsync.ts
+++ src/util/execAsync.ts
@@ -1,15 +1,15 @@
 import { exec } from 'child_process';
 
 
-
 export default async function execAsync(shellCommand: string, logCommand: boolean = false): Promise<string> {
   return new Promise<string>((resolve, reject) => {
     if (logCommand) {
       console.log(`Invoking shell command '${shellCommand}'`);
     }
-    exec(shellCommand, (error: Error | null, stdout: string) => {
+    exec(shellCommand, (error: Error | null, stdout: string, stderr: string) => {
       if (error) {
-        reject(stdout);
+        // @NOTE: most things print their errors to stdout because reasons
+        reject(stderr || stdout || "(EMPTY) Empty error - execAsync returned nothing");
       } else {
         resolve(stdout);
       }
